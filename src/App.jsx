cf2946f796505f2d0e1e80df731a23aa52b03fda
Rafaric <rafaelstrongoli@gmail.com>
2023-07-28 17:57:59 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Layout from "./pages/layout";
+
+function App() {
+  return (
+    <>
+      <Layout className="App">TASKO</Layout>
+    </>
+  );
+}
+
+export default App;
--- tasko/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <div className="App">TASKO</div>
-    </>
-  );
-}
-
-export default App;
